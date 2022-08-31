107f35c03bb20e2c3393de2c024b0978ef94c548
enigmacat <mumoxodom27@gmail.com>
2022-08-31 22:09:00 +1000

    add examples 001-007

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,3 @@
+﻿double numberA = 25;
+double numberB = 12;
+Console.WriteLine(numberA / numberB);
---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
