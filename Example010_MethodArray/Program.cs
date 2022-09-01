4097e77b9f42d84bea28c32d6495a5fdd0944457
enigmacat <mumoxodom27@gmail.com>
2022-09-01 21:39:52 +1000

    add examples 008-011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 88, 4, 3, 7, 9, 11, 32, 16, 7 };
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; //index + 1;
+}
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] Collection)
+
+{
+    int length = Collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Collection[index] = new Random().Next(1, 10);
+        //index = index +1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = indexOf(array, 7);
+Console.WriteLine(pos);
